b976cde0a3a0be272891fbf10b65f8142effed11
Bidossessi Sodonon <b_sodonon@sysadmin.colourball.com>
2011-06-28 21:30:50 +0100

    corrected file header

--- brss/functions.py
+++ brss/functions.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 #
-#       reader.py
+#       functions.py
 #       
 #       Copyright 2011 Bidossessi Sodonon <bidossessi.sodonon@yahoo.fr>
 #       
